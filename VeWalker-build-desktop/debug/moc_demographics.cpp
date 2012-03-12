ee3eaf11b0611cc5a8cb62d737147ea191099c01
Rob Sparrow <robsparrow@gmail.com>
2012-03-11 20:38:48 -0400

    Extended screen and made it modal

--- VeWalker-build-desktop/debug/moc_demographics.cpp
+++ VeWalker-build-desktop/debug/moc_demographics.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'demographics.h'
 **
-** Created: Sun Mar 11 20:25:29 2012
+** Created: Sun Mar 11 20:35:35 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
